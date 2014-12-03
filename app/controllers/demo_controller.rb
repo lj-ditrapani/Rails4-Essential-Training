0fb587fd5b7bbe6c58bc557ea5fdaef97b6c7540
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-03 15:04:20 -0500

    Complete up-to section 6 in course

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,29 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    # defaults to /demo/index.html
+    # but can use render to give custom template view
+    # render(:template => 'demo/hello')
+    # If no has, then :template is assumed
+    # render 'demo/hello'
+    # If no directory, then controller name is assumed
+    # render 'hello'
+  end
+
+  def hello
+    # render 'index'
+    @array = [1, 2, 3, 4, 5]
+    @id = params['id']  # could use :id as well
+    @page = params[:page]
+  end
+
+  def other_hello
+    # redirect_to(:controller => 'demo', :action => 'index')
+    # Defaults to current controller
+    redirect_to(:action => 'index')
+  end
+
+  def lynda
+    redirect_to('http://lynda.com')
   end
 end
