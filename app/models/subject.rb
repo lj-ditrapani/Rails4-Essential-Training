66a472f9c33551fbf3cec1f76c556ccfa1c135cf
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-09 06:35:00 -0500

    Completed section 7

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,7 @@
 class Subject < ActiveRecord::Base
+  scope :visible, -> { where visible: true }
+  scope :invisible, -> { where visible: false }
+  scope :sorted, -> { order 'subjects.position DESC' }
+  scope :newest_first, -> { order 'subjects.created_at ASC' }
+  scope :search, ->(query) { where ['name LIKE ?', "%#{query}%"] }
 end
