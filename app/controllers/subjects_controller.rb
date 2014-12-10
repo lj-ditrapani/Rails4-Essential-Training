503e316495f2cac98018abf0f48472dd117d6754
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2014-12-10 14:21:37 -0500

    Complete section 9, but not Challenge 2

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,66 @@
+# ----------------------------------------------------------------------
+class SubjectsController < ApplicationController
+  layout false
+
+  def index
+    @subjects = Subject.sorted
+  end
+
+  def show
+    @subject = Subject.find(params[:id])
+  end
+
+  def new
+    @subject = Subject.new(name: 'Default')
+  end
+
+  def create
+    # Instantiate a new object using form parameters
+    @subject = Subject.new(subject_params)
+    # Save the object
+    if @subject.save
+      # If save succeeds, redirect to the index action
+      flash[:notice] = 'Subject created successfully'
+      redirect_to(action: 'index')
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('new')
+    end
+  end
+
+  def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If update succeeds, redirect to the index action
+      flash[:notice] = 'Subject updated successfully'
+      redirect_to(action: 'show', id: @subject.id)
+    else
+      # If update fails, redisplay the form so user can fix problems
+      render('edit')
+    end
+  end
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject '#{subject.name}' deleted successfully"
+    redirect_to(action: 'index')
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :position, :visible)
+    # The below won't work because of strong params
+    # @subject = Subject.new(params[:subject])
+  end
+end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,7 +3,7 @@ class Subject < ActiveRecord::Base
 
   scope :visible, -> { where visible: true }
   scope :invisible, -> { where visible: false }
-  scope :sorted, -> { order 'subjects.position DESC' }
+  scope :sorted, -> { order 'subjects.position ASC' }
   scope :newest_first, -> { order 'subjects.created_at ASC' }
   scope :search, ->(query) { where ['name LIKE ?', "%#{query}%"] }
 end
